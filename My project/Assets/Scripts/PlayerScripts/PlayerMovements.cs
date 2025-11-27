dead837b8e49e19e6478a746c55f1b57ad2b8f64
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-11-27 13:56:30 +0200

    Add Player movement sound effects

--- My project/Assets/Scripts/PlayerScripts/PlayerMovements.cs
+++ My project/Assets/Scripts/PlayerScripts/PlayerMovements.cs
@@ -12,6 +12,8 @@ public class PlayerMovements : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public AudioSource walkingSound;
+
     private Vector3 velocity;
     private bool isGrounded;
 
@@ -30,6 +32,21 @@ public class PlayerMovements : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        // We check if the move vector has length (magnitude > 0.1) AND we are on the ground
+        if (move.magnitude > 0.1f && isGrounded)
+        {
+            // Only play if it is not ALREADY playing (prevents stuttering)
+            if (!walkingSound.isPlaying)
+            {
+                walkingSound.Play();
+            }
+        }
+        else
+        {
+            // Stop the sound if we stop moving OR if we jump
+            walkingSound.Stop();
+        }
 
         // Jump
         if (Input.GetButtonDown("Jump") && isGrounded)
