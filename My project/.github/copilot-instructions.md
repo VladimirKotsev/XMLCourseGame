f113d16cea0fa23cc41fa4b1749057a42ded9967
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-12-26 13:09:34 +0200

    Gun UI Selector

--- My project/.github/copilot-instructions.md
+++ My project/.github/copilot-instructions.md
@@ -1,4 +1,8 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: My project
 - Unity version: Unity 6000.2.10f1
+- Active game object:
+  - Name: Player
+  - Tag: Player
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- My project/Assets/Scripts/ShootingScripts/GunManager/GunManager.cs
+++ My project/Assets/Scripts/ShootingScripts/GunManager/GunManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
+    public GunUIManager uiManager;
+
     public List<GunInput> InputList;
     public List<Gun> Guns = new List<Gun>();
 
@@ -20,6 +22,38 @@ public class GunManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // chaka 1-2-3 kato input
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EquipGun(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EquipGun(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EquipGun(2);
+        }
+    }
+
+    private void EquipGun(int index)
+    {
+        if (index < Guns.Count)
+        {
+            this.CurrentGun = this.Guns[index];
+
+            // izvikva function change selected gun ui -drug script
+            // Checks if UI manager is assigned to avoid errors
+            if (uiManager != null)
+            {
+                // Replace 'UpdateGunUI' with the actual function name in your UI script
+                uiManager.UpdateGunUI(this.CurrentGun);
+            }
+        }
+    }
 
     private void Awake()
     {
@@ -39,8 +73,8 @@ public class GunManager : MonoBehaviour
             }
         }
         //this.CurrentGun = this.Guns[0]; //pistol
-        this.CurrentGun = this.Guns[1]; //sniper
-        //this.CurrentGun = this.Guns[2]; //shotgun
+        //this.CurrentGun = this.Guns[1]; //sniper
+        this.CurrentGun = this.Guns[2]; //shotgun
     }
 
     private void DisableGuns()
